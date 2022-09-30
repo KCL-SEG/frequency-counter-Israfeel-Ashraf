48313024962b5518982499e2796f47ba1c1a9444
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2022-09-30 12:01:37 +0100

    Finish task

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    
     frequencies = {}
+    
     # Your code goes here
+    for item in items:
+
+        x = str(item)
+
+        if frequencies.get(x) == None:
+            frequencies[x] = 1
+
+        else:
+            frequencies[x] += 1
+    
     return frequencies
+
+
+
